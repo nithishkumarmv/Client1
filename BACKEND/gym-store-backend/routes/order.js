7529c1616cbe091a7794c0d889f87ddba3150cae
nithishkumarmv <nithishmessi75400@gmail.com>
2025-05-10 12:28:57 +0530

    clear

--- BACKEND/gym-store-backend/routes/order.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const { placeOrder, getOrdersByUser } = require('../controllers/orderController');
-const router = express.Router();
-
-router.post('/place', placeOrder);
-router.get('/user/:id', getOrdersByUser);
-
-module.exports = router;
--- FRONTEND/gym-store-frontend/src/pages/ProductDetails.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-function ProductDetails() {
-  return <h2>Product Details Page</h2>;
-}
-
-export default ProductDetails;
